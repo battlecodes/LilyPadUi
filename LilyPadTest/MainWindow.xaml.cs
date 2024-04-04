060951185f2bf068d797f19fe549f5aefde5bace
battlecodes <calvin.algos@gmail.com>
2024-04-04 18:14:46 -0400

    feat(controls): Add Window and Button resource dictionaries

--- LilyPadTest/MainWindow.xaml.cs
+++ LilyPadTest/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,20 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LilyPadUi
+namespace LilyPadTest
 {
     /// <summary>
-    /// Interaction logic for UserControl1.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class UserControl1 : UserControl
+    public partial class MainWindow : Window
     {
-        public UserControl1()
+        public MainWindow()
         {
             InitializeComponent();
         }
     }
-
 }
